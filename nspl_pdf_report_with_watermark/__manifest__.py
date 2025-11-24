7c7271405ffea1f2d14bdcf5767bc700b8c03f25
Team Odoo NSPL <odoo_team@namahsoftech.com>
2025-11-24 10:32:48 +0530

    Add files via upload

--- nspl_pdf_report_with_watermark/__manifest__.py
+++ nspl_pdf_report_with_watermark/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Reports With Watermark',
-    'version': '18.0.1.0.0',
+    'version': '19.0.1',
     'category': 'Extra Tools',
     'summary': 'Add watermark to PDF reports',
     'description': """
